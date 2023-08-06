406372b3f5bb598e6754ab9c027cf159c03d551e
Keisuke Miura <toritoritori29@gmail.com>
2023-08-06 14:55:37 +0900

    feat: minimum echo server

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+extern crate libc;
+use std::mem;
+
+// Read these document before develpment.
+// https://doc.rust-jp.rs/rust-nomicon-ja/meet-safe-and-unsafe.html
+
+fn main() {
+    let addr = libc::sockaddr_in {
+        sin_family: libc::AF_INET as u16,
+        sin_port: (8080 as u16).to_be(),
+        sin_addr: libc::in_addr {
+            s_addr: libc::INADDR_ANY,
+        },
+        sin_zero: [0; 8],
+    };
+
+    println!("Start Server!");
+    unsafe{
+        let fd = libc::socket(libc::AF_INET, libc::SOCK_STREAM, 0);
+        let mut addr = mem::transmute::<libc::sockaddr_in, libc::sockaddr>(addr);
+        let mut addr_size = mem::size_of::<libc::sockaddr>() as u32;
+
+        //Bind and Listen
+        // https://linuxjm.osdn.jp/html/LDP_man-pages/man2/listen.2.html
+        libc::bind(fd, &addr, addr_size);
+        libc::listen(fd, 10);
+
+        // Accept
+        loop {
+            let accept_fd = libc::accept(fd,  &mut addr, &mut addr_size);
+            match accept_fd {
+                -1 => {
+                    println!("Error");
+                    continue;
+                },
+                _ => {
+                    // Read
+                    println!("Accept");
+                    let mut buf: [u8; 1024] = [0; 1024];
+                    libc::read(accept_fd, buf.as_mut_ptr() as *mut libc::c_void, 1024);
+                    let s = match String::from_utf8(buf.to_vec()) {
+                        Ok(v) => v,
+                        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                    };
+                    println!("Recv: {}", s);
+
+                    let send_str = format!("You send: {}", s);
+                    let mut send_buf = send_str.clone().into_bytes();
+                    println!("Send: {}", &send_str);
+
+                    libc::write(accept_fd, send_buf.as_mut_ptr() as *mut libc::c_void, send_buf.len());
+                    break;
+                }
+            }
+        }
+
+        // Shutdown
+
+        // Close
+        libc::close(fd);
+    }
+}
